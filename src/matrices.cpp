e711baad1232395ce0d43c7bbb99812f8e49367a
Tharusha Prabhashwara <wijerathnarktp.23@uom.lk>
2026-05-27 08:38:06 +0530

    complete matrices file

--- src/matrices.cpp
+++ src/matrices.cpp
@@ -33,3 +33,47 @@ mat4 Transpose(const mat4& mat) {
 	return temp;
 }
 
+mat2 operator* (const mat2& matrix, float scalar) {
+	mat2 temp;
+	for (int i = 0;i < 4;i++) {
+		temp.asArray[i] = matrix.asArray[i];
+	}
+	return temp;
+}
+
+mat3 operator* (const mat3& matrix, float scalar) {
+	mat3 temp;
+	for (int i = 0; i < 9; i++) {
+		temp.asArray[i] = matrix.asArray[i];
+	}
+	return temp;
+}
+
+bool Multiply(float* out, const float* matA, int aRow, int aCol, const float* matB, int bRow, int bCol) {
+	if (aCol != bRow) {
+		return false;
+	}
+	for (int i = 0; i < aRow; ++i) {
+		for (int j = 0; i < bCol; ++j) {
+			out[bCol * i + j] = 0.0f;
+			for (int k = 0; k < bRow; ++k) {
+				int a = aCol * i + k;
+				int b = bCol * k + j;
+				out[bCol * i + j] += matA[a] * matB[b];
+			}
+		}
+	}
+	return true;
+}
+
+mat2 operator* (const mat2& matA, const mat2& matB) {
+	mat2 temp;
+	Multiply(temp.asArray, matA.asArray, 2, 2, matB.asArray, 2, 2);
+	return temp;
+}
+
+mat3 operator* (const mat3& matA, const mat3& matB) {
+	mat3 temp;
+	Multiply(temp.asArray, matA.asArray, 3, 3, matB.asArray, 3, 3);
+	return temp;
+}
